8b6acbfacf2574bee1433e1e8357e6578b148bb9
Casey Link <unnamedrambler@gmail.com>
2024-02-28 18:25:15 +0100

    feat(davis): run migrations automatically

--- apps/davis/davis-entrypoint.sh
+++ apps/davis/davis-entrypoint.sh
@@ -4,6 +4,8 @@ if [ "$1" = "nginx" ]; then
     exec nginx -g 'daemon off;'
 else
     echo "Starting davis PHP-FPM..."
+    cd /var/www/davis
+    ./bin/console doctrine:migrations:migrate --no-interaction
     exec php-fpm
 fi
 
